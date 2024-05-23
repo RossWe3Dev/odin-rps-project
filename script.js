583294574ca7f3cdb3c1f9a52485211cab811d1f
Ross we3dev <168005603+RossWe3Dev@users.noreply.github.com>
2024-05-23 13:21:39 +0200

    Fix random numbers distribution
    Added getRandomInt function to get an even distribution
    of random numbers between 0, 1 and 2 with the max technique.
    This makes the getComputerNumber function a true random
    choice between 3 values (before the distribution was in
    favor of rock which had 0-33 = 34 integers, including the zero,
    against paper and scissors which only had 33 integers to
    chose from respectively).

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 console.log("Hello World!");
 
+//* function to get a random integer between 0, 1 and 2, for a total of 3 choices
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+};
+
 function getComputerChoice() {
-    let pcNumber = Math.floor(Math.random() * 100);
+    let pcNumber = getRandomInt(3);   //* Applied getRandomInt(3)
     let pcResult;
 
-    if (pcNumber >= 0 && pcNumber < 34) {
+    if (pcNumber === 0) {
         pcResult = 'rock';
-    } else if (pcNumber > 33 && pcNumber < 67) {
+    } else if (pcNumber === 1) {
         pcResult = 'paper';
     } else {
         pcResult = 'scissors';
